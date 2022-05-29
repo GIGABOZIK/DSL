664d1d960ee36c8532e207c18d2829736180cc75
GIGABOZIK <ivan_belyaev_2002@mail.ru>
2022-05-29 16:17:07 +0300

    Начал писать интерпретатор !!!!

--- src/DSL/Interpreter.java
+++ src/DSL/Interpreter.java
@@ -27,13 +27,12 @@ public class Interpreter {
         System.out.println('\n' + node.getClass().getName());
         //
         if (UnOpNode.class.equals(node.getClass())) {
-            //
             if (((UnOpNode) node).getOperator().type().equals("KW_WRITE")) {
                 System.out.println(executeNode(((UnOpNode) node).getArg()));
             }
         }
         if (BinOpNode.class.equals(node.getClass())) {
-            //
+            // Commit here (:
         }
         return null;
     }
