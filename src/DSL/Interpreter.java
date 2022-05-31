5d185f1b765ac7fbda94cccf2bd6b95201a69b3a
GIGABOZIK <ivan_belyaev_2002@mail.ru>
2022-05-31 11:10:20 +0300

    Добавил If-Else

--- src/DSL/Interpreter.java
+++ src/DSL/Interpreter.java
@@ -15,24 +15,73 @@ public class Interpreter {
     public void execute() {
         for (Node node : rootNode.getCodeChain()) {
             executeNode(node);
-//            printNode(node); // Прост
         }
     }
 
-    private void printNode(Node node) {
-        // Просто вывести все узлы
-    }
-
     private String executeNode(Node node) {
-        System.out.println('\n' + node.getClass().getName());
+//        System.out.println('\n' + node.getClass().getName());
         //
-        if (UnOpNode.class.equals(node.getClass())) {
+        if (node.getClass().equals(IntNode.class)) {
+            return ((IntNode) node).getNumber().value();
+        }
+        if (node.getClass().equals(StringNode.class)) {
+            return ((StringNode) node).getString().value();
+        }
+        if (node.getClass().equals(IdNode.class)) {
+            return scope.get(((IdNode) node).getIdent().value());
+        }
+        if (node.getClass().equals(UnOpNode.class)) {
             if (((UnOpNode) node).getOperator().type().equals("KW_WRITE")) {
                 System.out.println(executeNode(((UnOpNode) node).getArg()));
             }
         }
-        if (BinOpNode.class.equals(node.getClass())) {
-            // Commit here (:
+        if (node.getClass().equals(BinOpNode.class)) {
+            if ("ASSIGN_OP".equals(((BinOpNode) node).getOperator().type())) {
+                IdNode idNode = (IdNode) ((BinOpNode) node).getLeftOperand();
+                String asValue = executeNode(((BinOpNode) node).getRightOperand());
+                scope.put(idNode.getIdent().value(), asValue);
+//                scope.put(leftOperand, rightOperand); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            } else {
+                int leftOperand = Integer.parseInt(executeNode(((BinOpNode) node).getLeftOperand()));
+                int rightOperand = Integer.parseInt(executeNode(((BinOpNode) node).getRightOperand()));
+                switch (((BinOpNode) node).getOperator().type()) {
+                    case "ADD_OP" -> { return String.valueOf(leftOperand + rightOperand); }
+                    //
+                    case "SUB_OP" -> { return String.valueOf(leftOperand - rightOperand); }
+                    case "MUL_OP" -> { return String.valueOf(leftOperand * rightOperand); }
+                    case "DIV_OP" -> { return String.valueOf(leftOperand / rightOperand); }
+                    //
+                    case "COMP_L_EQ" -> { return String.valueOf(leftOperand <= rightOperand); }
+                    case "COMP_M_EQ" -> { return String.valueOf(leftOperand >= rightOperand); }
+                    case "COMP_LESS" -> { return String.valueOf(leftOperand < rightOperand); }
+                    case "COMP_MORE" -> { return String.valueOf(leftOperand > rightOperand); }
+                    case "COMP_EQ" -> { return String.valueOf(leftOperand == rightOperand); }
+                    case "COMP_NEQ" -> { return String.valueOf(leftOperand != rightOperand); }
+                }
+            }
+        }
+
+        // FOR, WHILE, IF-ELSE
+        if (node.getClass().equals(IfElseNode.class)) {
+            if ("true".equals(executeNode(((IfElseNode) node).getCondition()))) {
+                for (Node ifExpr : ((IfElseNode) node).getIfExprs()) {
+                    executeNode(ifExpr);
+                }
+            } else {
+                if ("false".equals(executeNode(((IfElseNode) node).getCondition()))) {
+                    for (Node elseExpr : ((IfElseNode) node).getElseExprs()) {
+                        executeNode(elseExpr);
+                    }
+                } else System.out.println("Condition Error!");
+            }
+        }
+        if (node.getClass().equals(ForNode.class)) {
+            executeNode(((ForNode) node).getInit());
+            // ...
+        }
+        if (node.getClass().equals(WhileNode.class)) {
+            executeNode(((WhileNode) node).getCondition());
+            // ...
         }
         return null;
     }
